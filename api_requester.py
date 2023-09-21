6f811be4e0d16afeede21ae260b0d18d7f84725f
felipe <felipetdecarli@gmail.com>
2023-09-21 18:58:20 -0300

    fmt: rm unused print and comments

--- api_requester.py
+++ api_requester.py
@@ -1,6 +1,5 @@
 import requests
 import os
-import urllib.parse
 
 API_ENDPOINT = 'http://storage.scrapinghub.com/items/'
 API_KEY = os.environ["API_KEY"]
@@ -9,14 +8,11 @@ def send_request(path_param, params):
     headers = {
         'Authorization': f"Basic {API_KEY}"
     }
-    # Add format=json to params
+    
     params['format'] = 'json'
-    # Use path_param in the URL
+    
     url = f"{API_ENDPOINT}{path_param}"
     response = requests.get(url, params=params, headers=headers)
-    
-    print(response.url)
-    print(urllib.parse.unquote_plus(response.url))
     
     if response.status_code == 200:
         return response.json()
