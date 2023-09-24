9b1868081e2875141ee565c37a392bd2ab649754
felipdc <felipe@HP-LaptopFelipe>
2023-09-23 21:42:24 -0300

    fix: api key basic auth

--- api_requester.py
+++ api_requester.py
@@ -3,16 +3,12 @@ import requests
 API_ENDPOINT = 'http://storage.scrapinghub.com/items/'
 
 def send_request(path_param, params, api_key):
-    headers = {
-        'Authorization': f"Basic {api_key}"
-    }
-    
     params['format'] = 'json'
     
     url = f"{API_ENDPOINT}{path_param}"
-    response = requests.get(url, params=params, headers=headers)
-    
+    response = requests.get(url, params=params, auth=(api_key, ''))
+
     if response.status_code == 200:
         return response.json()
     else:
-        return f"Error {response.status_code}: {response.text}"
+        return f'Error {response.status_code}: {response.text}'
