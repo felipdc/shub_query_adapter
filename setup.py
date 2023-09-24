a17b0cbc7ba4611d49cd6c602c56d005d0c74443
felipe <felipetdecarli@gmail.com>
2023-09-23 23:41:48 -0300

    feat: udpate version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shub_query_adapter",
-    version="0.1",
+    version="0.2",
     packages=find_packages(),
     install_requires=[
         "requests"
