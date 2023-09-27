5d438a7b20571a40a0f37dd57143a70cf2c5b87e
felipe <felipetdecarli@gmail.com>
2023-09-27 19:09:49 -0300

    bump version to 0.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="shub_query_adapter",
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     install_requires=[
         "requests"
